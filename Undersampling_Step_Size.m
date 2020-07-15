a264b1255e3e2ff703cf2e9664d11cce60660a24
RellieGoddard <52534240+RellieGoddard@users.noreply.github.com>
2020-07-15 11:20:26 +0100

    Add files via upload

---
+++ Undersampling_Step_Size.m
@@ -0,0 +1,103 @@
+%% Undersampling_Step_size - tests if the spatial resolution is high enough 
+% Rellie Goddard, July 2019
+% Required functions:
+% *ProcessEBSD_fun.m
+% *LinearIntercepts_fun.m
+% *Undersampling.fun.m
+
+% Required user inputs:
+%       nx, Int_max, direname, sample_name, Title, header_size,  gb_min,
+%       sg_min, cutoff, phase, crystal, test, Phase_map, Band_contrast
+%           nx: no. of line intercepts, chosen based on analysis from No_intercepts_check.m.
+%           Int_max: the number of times you want to reduce the step-size. The step-size at an iteration will be Int_max multiplied by the original step-size. 
+%           direname: equivalent to  pname
+%           sample_name: equivalent to fname but without the .ctf.
+%           Title: Title for figure 
+%           Header_size: Number of lines, up to and including the line starting with ‘phase’ in the .ctf file (open in Notepad)
+%           gb_min & sg_min: grain size and subgrain size, used for constructing maps
+%           cutoff: minimum misorientation angle used to define a subgrain boundary, if using Goddard subgrain-size piezometer cuttoff = 1. 
+%           phase: the phase you want to measure subgrains in 
+%           crystal: crystal system on the phase in question
+%           Phase_map: to output a phase map = 1 if not = 0
+%           Band_contrast: to output a band contrast map = 1 if not = 0 
+%
+%       Outputs: Figure showing the Intercept variation factor plotted
+%       against the number of pixels per intercept. See Goddard et al., 2020
+%       for an explanation of the variables. 
+%
+%
+% The test is successful if the measured mean intercept length is not sensitive to the effective step size. 
+% The presence of an asymptote at an intercept variation factor of 1 is evidence that step size is small enough to capture the mean intercept length. 
+% If such an asymptote doesn't exist then either re-map the sample, using a smaller step size, or use the 
+% subgrain-size stress measurement as a lower bound.  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Data import
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+close all 
+clear all 
+
+%USER INPUT: Number of intercepts
+nx = [];
+ny = nx;
+
+%USER INPUT: Maximum times of reducing the step-size 
+Int_max = []; 
+
+%File Location 
+dirname = 'yourPath';
+
+% USER INPUT: Enter .ctf file name (excluding the .ctf)
+sample_name = 'yourFileName';
+
+% USER INPUT: Title for graph
+Title = 'yourTitle';
+
+%% Define the parameters 
+
+% USER INPUT: Header size of the original head file. Open text file. 
+header_size = []; 
+
+% USER INPUT: misorientation angles for SG and GS and cutoff for
+% SG_piezometer
+% For the Goddard 2020 subgrain piezometer set sg_min and cutoff to 1 degree. 
+
+gb_min = [];
+sg_min = [];
+cutoff = []; 
+
+% USER INPUT Phase, must match that in the CS file.
+
+phase = 'yourPhase';
+
+% USER INPUT: Crystal system 
+
+%  Common crystal systems 
+% 'Quartz = trigonal'   'Calcite = trigonal'    
+% 'Enstatite  Opx AV77 = orthorhombic'  'Forsterite = orthorhombic'
+
+crystal = 'yourCrystalSystem';
+% test, if you want to do a smaller data set to test the code 1 = YES, 0 =
+% NO
+
+test = [];
+
+% Following figures will be created if 1 and not if 0
+Phase_map = [];
+Band_contrast = [];
+
+Input_CTF = [dirname sample_name '.ctf'];
+A = importdata(Input_CTF);
+[fname_new, stepx_all, Step_size_SG_size] = undersampling_fun(Int_max, dirname, sample_name, header_size,gb_min,sg_min,test, Phase_map, Band_contrast, nx, ny, cutoff, phase, crystal);
+
+save('stepx_all_264','stepx_all');
+save('Step_size_SG_size_264','Step_size_SG_size');
+
+close all 
+
+figure 
+    plot(Step_size_SG_size(1)./stepx_all, Step_size_SG_size/Step_size_SG_size(1),'-o', 'LineWidth', 3, 'color', [0.49 0.34 0.75], 'MarkerSize', 7);
+    set(0,'DefaultAxesFontSize',15,'DefaultAxesFontName','helvetica','DefaultAxesTickLength',[.03 .02],'DefaultAxesLineWidth',2);
+    set(gca,'fontsize',15);
+    ylabel('Intercept variation factor ,\bf\lambda\rm/\bf\lambda\rm_b_e_s_t', 'FontSize',15);
+    xlabel('Pixels per intercept, \bf\lambda\rm_b_e_s_t/step size', 'FontSize', 15);
+    title(Title, 'FontSize', 15)
---
+++ undersampling_fun.m
@@ -0,0 +1,78 @@
+
+function [fname_new, stepx_all, Step_size_SG_size] = undersampling_fun(Int_max, dirname, sample_name, header_size,gb_min,sg_min,test, Phase_map, Band_contrast, nx, ny, cutoff, phase, crystal);
+
+Step_size_SG_size = [];
+stepx_all = [];
+
+%%%%%%User Inputs%%%%%%
+for Int = 1:1:Int_max;
+fname_new = [sample_name '_int' num2str(Int)];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+Input_CTF = [dirname sample_name '.ctf'];
+data = importdata(Input_CTF,';');
+
+%Original numbers of rows and columns
+nc_or =(str2num(data{5}(8:size(data{5},2))));
+nr_or =(str2num(data{6}(8:size(data{6},2))));
+
+%New numbers of rows and columns
+nc = ceil(nc_or/Int);
+nr = ceil(nr_or/Int);
+
+
+%Indices of points to use
+x=[1:Int:nc_or]';
+ind=[];
+for i=1:nr
+    temp=x+(nc_or*Int)*(i-1);
+    ind=[ind;temp];
+end
+
+%Modify step size
+stepx = str2num(data{7}(7:end))*Int;
+stepy = str2num(data{8}(7:end))*Int;
+data{7}(7:end) = '';
+data{7}(7:7+size(num2str(stepx),2)-1) = num2str(stepx);
+data{8}(7:end) = '';
+data{8}(7:7+size(num2str(stepy),2)-1) = num2str(stepy);
+
+stepx_all = [stepx_all, stepx];
+
+%Modify number of cells
+data{5}(8:end) = '';
+data{5}(8:8+size(num2str(nc),2)-1) = num2str(nc);
+data{6}(8:end) = '';
+data{6}(8:8+size(num2str(nr),2)-1) = num2str(nr);
+
+%Modify rows of data
+data_new = data(1:length(ind)+ header_size);
+data_new(header_size+1:end) = data(ind+header_size);
+
+
+%Write to new ctf file
+fileid = fopen([fname_new '.ctf'],'w');
+fprintf(fileid,'%s\r\n',data_new{:,:})
+fclose(fileid)
+
+fname = [fname_new '.ctf'];
+[ebsd,grains,subgrains] = ProcessEBSD_fun(fname,gb_min,sg_min, phase, test, Phase_map, Band_contrast);
+[Mean_Lengths_X,Mean_Lengths_Y, lengths_x, lengths_y] = LinearIntercepts_fun(ebsd,nx,ny,cutoff,phase,crystal);
+
+d_h = lengths_x;
+d_v = lengths_y;
+d = [d_h;d_v];
+%calculate the arithmetic mean 
+a_mean_RG = sum(d)/length(d);
+
+Step_size_SG_size = [Step_size_SG_size, a_mean_RG];
+end 
+end 
+
+
+
+
+
+
